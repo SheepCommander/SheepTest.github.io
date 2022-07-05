0027547f4d7527b63d4a22ea4dcd359c5138abba
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-05 10:29:21 -0700

    stuff

--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 questions =  [
     {"id": 0, "question": "We shouldn't be able to lewd server members.", "effects": {"right": 1}},
     {"id": 1, "question": "I love werdz.", "effects": {"auth": 1}},
+    {"id": 2, "question": "Femboys are cute.", "effects": {"right": -1}},
 ];
