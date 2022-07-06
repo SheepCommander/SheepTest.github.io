4da02fa9ba02bd8c64084f08311e7c59d9acc2f4
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-05 23:56:37 -0700

    reorder!

--- questions.js
+++ questions.js
@@ -1,4 +1,13 @@
 questions =  [
-    {"id": 0, "effects": {"auth": 10, "right": 0}, "question": "I am a server mod (pls put Neutral if you're not)"},
+    {"id": 0, "effects": {"auth": 10}, "question": "I am a server mod (pls put Neutral if you're not)"},
     {"id": 1, "effects": {"auth": 1, "right": -1}, "question": "Werdz is best girl"},
+    {"id": 2, "effects": {"auth": 1}, "question": "I rarely break server rules"},
+    {"id": 3, "effects": {"auth": 0.5}, "question": "I can list every mod"},
+    {"id": 4, "effects": {"right": -1}, "question": "I hornypost often"},
+    {"id": 5, "effects": {"right": -1}, "question": "I hornypost other discord members"},
+    {"id": 6, "effects": {"right": 1}, "question": "I hate hornyposters"},
+    {"id": 7, "effects": {"right": -0.5}, "question": "Fourth is cute"},
+    {"id": 8, "effects": {"right": -0.5}, "question": "Rushers is cute"},
+    {"id": 9, "effects": {"right": -0.5}, "question": " is cute"},
+    {"id": 10, "effects": {"auth": 1}, "question": "mods are good and keep the server in check"},
 ];
