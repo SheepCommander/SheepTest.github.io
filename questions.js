199fe824d0983f7c55da5540d677b5344f6baf80
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-05 23:48:59 -0700

    test different order

--- questions.js
+++ questions.js
@@ -1,12 +1,3 @@
 questions =  [
-    {"id": 0, "question": "I am a server mod (pls put Neutral if you're not)", "effects": {"auth": 100}},
-    {"id": 1, "question": "Werdz is best girl", "effects": {"auth": 1}},
-    {"id": 2, "question": "I rarely break server rules", "effects": {"auth": 1}},
-    {"id": 3, "question": "I can list every mod", "effects": {"auth": 1}},
-    {"id": 4, "question": "I hornypost often", "effects": {"right": -1}},
-    {"id": 5, "question": "I hornypost other discord members", "effects": {"right": -1}},
-    {"id": 6, "question": "I hate hornyposters", "effects": {"right": 1}},
-    {"id": 7, "question": "Fourth is cute", "effects": {"right": -0.5}},
-    {"id": 8, "question": "Rushers is cute", "effects": {"right": -0.5}},
-    {"id": 9, "question": " is cute", "effects": {"right": -0.5}},
+    {"id": 0, "effects": {"auth": 10}, "question": "I am a server mod (pls put Neutral if you're not)"},
 ];
