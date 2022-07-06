58417dc36b6e94ca7cf290a73aea9bf03581d789
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-05 23:39:36 -0700

    werwer

--- questions.js
+++ questions.js
@@ -1,4 +1,12 @@
 questions =  [
-    {"id": 0, "question": "I am a server mod Auth +100 test", "effects": {"auth": 100}},
-    {"id": 1, "question": "Auth +1 test", "effects": {"auth": 1}},
+    {"id": 0, "question": "I am a server mod (pls put Neutral if you're not)", "effects": {"auth": 100}},
+    {"id": 1, "question": "Werdz is best girl", "effects": {"auth": 1}},
+    {"id": 2, "question": "I rarely break server rules", "effects": {"auth": 1}},
+    {"id": 3, "question": "I can list every mod", "effects": {"auth": 1}},
+    {"id": 4, "question": "I hornypost often", "effects": {"right": -1}},
+    {"id": 5, "question": "I hornypost other discord members", "effects": {"right": -1}},
+    {"id": 6, "question": "I hate hornyposters", "effects": {"right": 1}},
+    {"id": 7, "question": "", "effects": {"right": 1}},
+    {"id": 8, "question": "", "effects": {"right": -1}},
+    {"id": 9, "question": "", "effects": {"auth": -1}},
 ];
