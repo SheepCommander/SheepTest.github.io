e8031918e2b38a366c2bd46e0549efd0125af910
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-06 00:29:45 -0700

    last

--- questions.js
+++ questions.js
@@ -1,5 +1,6 @@
 questions =  [
-    {"id": 0, "effects": {"auth": 10},              "question": "I am a server mod (pls put Neutral if you're not)"},
+    //{"id": 0, "effects": {"auth": 10},              "question": "I am a server mod (pls put Neutral if you're not)"},
+    {"id": 0, "effects": {"auth": 1, "right": -0.1},"question": "GLORY TO WERDZISTAN"},
     {"id": 1, "effects": {"auth": 1, "right": -1},  "question": "Werdz is best girl"},
     {"id": 2, "effects": {"auth": 1},               "question": "I rarely break server rules"},
     {"id": 3, "effects": {"auth": 0.5},             "question": "I can list every mod"},
@@ -9,5 +10,6 @@ questions =  [
     {"id": 7, "effects": {"right": -0.5},           "question": "Fourth is cute"},
     {"id": 8, "effects": {"right": -0.5},           "question": "Rushers is cute"},
     {"id": 9, "effects": {"right": -0.5},           "question": " is cute"},
-    {"id": 10, "effects": {"auth": 1},              "question": "mods are good and keep the server in check"},
+    {"id": 10, "effects": {"auth": 1},              "question": "1984 is necesarry"},
+    {"id": 11, "effects": {"auth": -0.5},           "question": "I inflict shame on other people"},
 ];
