f46e986fddab185837c35e3e465cc75a741b4a8f
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-05 23:51:14 -0700

    attempt two change order

--- questions.js
+++ questions.js
@@ -1,3 +1,4 @@
 questions =  [
-    {"id": 0, "effects": {"auth": 10}, "question": "I am a server mod (pls put Neutral if you're not)"},
+    {"id": 0, "effects": {"auth": 10, "right": 0}, "question": "I am a server mod (pls put Neutral if you're not)"},
+    {"id": 1, "effects": {"auth": 1, "right": -1}, "question": "Werdz is best girl"},
 ];
