3fea553699136bc31b9e1ef22a22ff4506c123ca
Sheep Commander <66646514+SheepCommander@users.noreply.github.com>
2022-07-05 23:28:11 -0700

    test

--- questions.js
+++ questions.js
@@ -1,13 +1,4 @@
 questions =  [
-    {"id": 0, "question": "We shouldn't lewd server members.", "effects": {"right": 1}},
-    {"id": 1, "question": "I love werdz.", "effects": {"auth": 1}},
-    {"id": 2, "question": "I love werdzchamasama degozarudesuwa~, high king and ruler of vtplace", "effects": {"auth": 1}},
-    {"id": 3, "question": "I am lac", "effects": {"right": 10, "auth": 1}},
-    {"id": 4, "question": "", "effects": {"right": 1}},
-    {"id": 5, "question": "", "effects": {"right": 1}},
-    {"id": 6, "question": "", "effects": {"right": 1}},
-    {"id": 7, "question": "", "effects": {"right": 1}},
-    {"id": 8, "question": "", "effects": {"right": 1}},
-    {"id": 9, "question": "", "effects": {"right": 1}},
-    {"id": 10, "question": "", "effects": {"right": 1}},
+    {"id": 0, "question": "I am a server mod Auth +100 test", "effects": {"auth": 100}},
+    {"id": 1, "question": "Auth +1 test", "effects": {"auth": 1}},
 ];
